633de8f5f31709822b515b4e35ac56db15a98d1a
Valchuuus <valehetaliana123@gmail.com>
2025-08-15 18:13:24 -0400

    Update data.js

--- src/js/data.js
+++ src/js/data.js
@@ -15,9 +15,9 @@ const dataSet = {};
  * 
  * @example '2018-02-20'
 */
-let dataSetVersion = '';
+let dataSetVersion = '2025-08-15';
 
 /**
  * Image root, will be appended to the start of every image URL.
  */
-const imageRoot = 'https://i.imgur.com/';
+const imageRoot = "https://raw.githubusercontent.com/Valchuuus/MangadeWeonesSorter/main/src/assets/Chars/";
